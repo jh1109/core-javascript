56331dfd65fe572232efe8b996eacec1e5f13e64
jh1109 <nohjuhee227@gmail.com>
2023-01-11 17:39:08 +0900

    스타일과 클래스 복습

--- client/chapter/dom/05.styling.js
+++ client/chapter/dom/05.styling.js
@@ -10,6 +10,7 @@ const first = getNode('.first');
 
 // - className – 클래스 전체를 문자열 형태로 반환해주는 프로퍼티로 클래스 전체를 관리할 때 
 console.log(first.className);
+console.log(first.classList);
 // console.log(first.className = "first hello");
 // console.log(first.className = 'hola');
 // - classList – 클래스 하나를 관리할 수 있게 해주는 메서드로 개별 클래스를 조작할 때 유용
@@ -47,31 +48,8 @@ console.log(size);
 */ 
 
 // 계산되어 있지 않은 값 가져오기
-function getCss(node,prop){
-  if(typeof node === 'string'){
-    node = getNode(node);
-  }
-
-  if(!(prop in document.body.style)){
-    syntaxError('getCSS 함수의 두 번째 인자인 prop은 유효한 css 속성이 아닙니다.')
-  }
-  return getComputedStyle(node)[prop]
-  
-}
 
 // 대상에게 원하는 css 속성을 추가
-function setCss(node, prop, value){
-  if (typeof node === 'string') node = getNode(node);
-
-  if(!(prop in document.body.style)){
-    syntaxError('getCSS 함수의 두 번째 인자인 prop은 유효한 css 속성이 아닙니다.')
-  }
-
-  if (!value) {
-    syntaxError('setCss 함수의 세 번째 인자는 필수값 입니다.');
-  }
-  node.style[prop] = value;
-}
 
 console.log( getCss('.first','font-size') );
 setCss('.first', 'color', 'white')
@@ -79,15 +57,11 @@ setCss('.first', 'color', 'white')
 
 // 둘을 한번에 쓰는 함수를 만들어볼까..?
 
-function css(node, prop, value) {
-  return !value ? getCss(node,prop) : setCss(node,prop,value);
-}
-
-
 css('.first', 'font-size', '100px'); //set
 
 // 왜 여전히 32px가 나오지?
-// 비동기로 처리..
+// 바로 앞에 100px로 동작하고 font-size의 값을 불러오는데 시간이 좀 필요하기 때문..
+// 그래서 비동기로 처리..
 setTimeout(()=> {
   console.log(css('.first', 'font-size')); // get
 
