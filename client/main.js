7ee4af610cac218c354181e88c7fa33ad6dd1721
jh1109 <nohjuhee227@gmail.com>
2023-01-16 10:48:20 +0900

    기본 파일 셋팅

--- client/main.js
+++ client/main.js
@@ -1,1 +1,10 @@
-console.log(window);
+import { diceAnimation, getNode } from "./lib/index.js";
+
+const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+
+
+const handlerRollingDice = () => {
+  diceAnimation()
+}
+
+rollingDiceButton.addEventListener('click', handlerRollingDice);
