a31c9bc420c0e6ef716ffaac8668670c153cb2b7
jh1109 <nohjuhee227@gmail.com>
2023-01-16 14:03:24 +0900

    레코드 리스트 보이기

--- client/main.js
+++ client/main.js
@@ -1,10 +1,40 @@
-import { diceAnimation, getNode } from "./lib/index.js";
+import { diceAnimation, getNode, getNodes, disableElement, enableElement, visibleElement, invisibleElement } from "./lib/index.js";
 
-const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// 배열의 구조 분해 할당
+const [rollingDiceButton, recordButton, resetButton] = getNodes('.buttonGroup > button');
+// const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// const recordButton = getNode('.buttonGroup > button:nth-child(2)');
+const recordListWrapper = getNode('.recordListWrapper');
 
 
-const handlerRollingDice = () => {
-  diceAnimation()
+// IIFE
+const handleRollingDice = ( () => {
+  let stopAnimation;
+  let isRolling = false;
+
+  return () => {
+    if(!isRolling){
+      stopAnimation = setInterval(diceAnimation, 200);
+      // disableElement(recordButton);
+      // disableElement(resetButton);
+    } else{
+      clearInterval(stopAnimation);
+      enableElement(recordButton);
+      enableElement(resetButton);
+    }
+    isRolling = !isRolling;
+  }
+
+  })()
+
+rollingDiceButton.addEventListener('click', handleRollingDice);
+
+function handleRecord(){
+  visibleElement(recordListWrapper);
 }
+recordButton.addEventListener('click',handleRecord);
 
-rollingDiceButton.addEventListener('click', handlerRollingDice);
+function handleReset(){
+  invisibleElement(recordListWrapper);
+}
+resetButton.addEventListener('click',handleReset);
