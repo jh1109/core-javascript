f142b90ee1a610f2a44d9783698ba15f4e9e7b91
jh1109 <nohjuhee227@gmail.com>
2023-01-19 16:08:17 +0900

    textField값 storage에 저장

--- client/main.js
+++ client/main.js
@@ -1,1 +1,18 @@
-import {  } from "./lib/index.js";
+import { getNode, loadStorage, saveStorage } from "./lib/index.js";
+
+
+const textField = getNode('#textField');
+const deleteButton = getNode('input[value="삭제"]');
+
+loadStorage('area').then(res=>{
+  textField.value = res;
+})
+
+function inputHandler(){
+  saveStorage('area', textField.value);
+}
+
+textField.addEventListener('input', inputHandler);
+// 값이  작성되는 지를 확인하는 method > input
+
+// 삭제버튼을 누르면 storage가 다 날라가는거 해보기
